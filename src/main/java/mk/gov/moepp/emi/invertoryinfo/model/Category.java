1deaa35d7474647202e9a1d9fde8896a7bcd692f
ristovkiril <ristov.kiril@yahoo.com>
2020-10-01 14:52:44 +0200

    subcategory

--- src/main/java/mk/gov/moepp/emi/invertoryinfo/model/Category.java
+++ src/main/java/mk/gov/moepp/emi/invertoryinfo/model/Category.java
@@ -1,11 +1,15 @@
 package mk.gov.moepp.emi.invertoryinfo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -20,6 +24,14 @@ public class Category {
     private int prefix;
 
     //subcategory?
+    @ManyToOne
+    @JoinColumn(name = "subcategory", referencedColumnName = "id")
+    @JsonManagedReference
+    private Category subcategory;
+
+    @OneToMany(mappedBy = "subcategory",  cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<Category> children = new HashSet<>();
 
     @ManyToMany
     private List<Analysis> analyses;
