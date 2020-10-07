2e4927f2f12e5e1c25d100ff2b1817d623dd340a
angelajaneva <janeva.angela@live.com>
2020-10-07 16:38:18 +0200

    nav elements

--- src/frontend/app/components/common/Navigation.js
+++ src/frontend/app/components/common/Navigation.js
@@ -4,6 +4,14 @@ import { Link, Location } from 'react-router';
 
 class Navigation extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          showCategories: false
+        };
+    }
+
     componentDidMount() {
         const { menu } = this.refs;
         $(menu).metisMenu();
@@ -17,6 +25,20 @@ class Navigation extends Component {
         return this.props.location.pathname.indexOf(routeName) > -1 ? "nav nav-second-level collapse in" : "nav nav-second-level collapse";
     }
 
+    showCategoriesList = (e) => {
+        e.preventDefault();
+        this.setState({
+            showCategories: true
+        });
+    };
+
+    showYearsList = (e) => {
+        e.preventDefault();
+        this.setState({
+            showCategories: false
+        });
+    };
+
     render() {
         return (
             <nav className="navbar-default navbar-static-side" role="navigation">
@@ -26,7 +48,7 @@ class Navigation extends Component {
                              </span>
                                 <a data-toggle="dropdown" className="dropdown-toggle" href="#">
                             <span className="clear"> <span className="block m-t-xs"> <strong className="font-bold">Example user</strong>
-                             </span> <span className="text-muted text-xs block">Example position<b className="caret"></b></span> </span> </a>
+                             </span> <span className="text-muted text-xs block">Example position<b className="caret"/></span> </span> </a>
                                 <ul className="dropdown-menu animated fadeInRight m-t-xs">
                                     <li><a href="#"> Logout</a></li>
                                 </ul>
@@ -35,12 +57,28 @@ class Navigation extends Component {
                                 IN+
                             </div>
                         </li>
-                        <li className={this.activeRoute("/main")}>
-                            <Link to="/main"><i className="fa fa-th-large"></i> <span className="nav-label">Main view</span></Link>
+                        <li className={this.activeRoute("/main")} onClick={this.showYearsList}>
+                            <Link to="/main"><i className="fa fa-list-ul"/>
+                                <span className="nav-label">Години</span>
+                                <i className={`pull-right ${!this.state.showCategories ? 'fa fa-angle-down' : ' fa fa-angle-left'}`}/>
+                            </Link>
                         </li>
-                        <li className={this.activeRoute("/minor")}>
-                            <Link to="/minor"><i className="fa fa-th-large"></i> <span className="nav-label">Minor view</span></Link>
+
+                        <li className={this.activeRoute("/minor")} onClick={this.showCategoriesList} >
+                            <Link to="/minor"><i className="fa fa-sitemap"/> <span className="nav-label">Категории</span>
+                                <i className={`pull-right ${this.state.showCategories ? 'fa fa-angle-down' : ' fa fa-angle-left'}`}/>
+                            </Link>
                         </li>
+                        { this.state.showCategories ?
+                            (
+                                    <li className={this.activeRoute("/minor")} >
+                                        <Link to="/minor"> <span className="nav-label">Proba1</span></Link>
+                                    </li>
+                            )
+
+                            : null
+                        }
+
                     </ul>
 
             </nav>
