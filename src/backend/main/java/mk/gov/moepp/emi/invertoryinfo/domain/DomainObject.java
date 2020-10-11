c296db466c115ab27ccb0d166fe8d5623eef8555
ristovkiril <ristov.kiril@yahoo.com>
2020-10-11 17:06:47 +0200

    Domain

---
+++ src/backend/main/java/mk/gov/moepp/emi/invertoryinfo/domain/DomainObject.java
@@ -0,0 +1,6 @@
+package mk.gov.moepp.emi.invertoryinfo.domain;
+
+import java.io.Serializable;
+
+public interface DomainObject extends Serializable {
+}
---
+++ src/backend/main/java/mk/gov/moepp/emi/invertoryinfo/domain/ValueObject.java
@@ -0,0 +1,4 @@
+package mk.gov.moepp.emi.invertoryinfo.domain;
+
+public interface ValueObject extends DomainObject {
+}
